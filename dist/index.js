7d4689c9c509e64f7f6f1ee61a892880d0dbfc4d
Nelly <nelly.babillon@gmail.com>
2025-02-19 11:10:57 +0100

    Grid demineur

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,14 @@ function hello(who) {
      return 'Hello, ' + (who || WORLD);
 }
 
-console.log("JS-Ready");
-window.addEventListener('load', function() {
-     document.body.textContent = hello();
-});
+const mainGrid = document.querySelector('main')
+
+if (mainGrid) {
+     for (let j = 0; j < 20*20; j++) {
+          const cell = document.createElement('div')
+          cell.classList.add('cell')
+          mainGrid.appendChild(cell)
+     }
+} else {
+     console.error("Main grid element not found");
+}
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,20 @@ export function hello(who:string = WORLD): string {
 
 console.log("JS-Ready");
 
-window.addEventListener("load", ()=>{
-  document.body.textContent = hello();
-});
+
+// const mainGrid = document.querySelector('main')
+
+// if (mainGrid) {
+//   for(let j = 0; j < 20; j++) {
+//     const row = document.createElement('div')
+//     row.classList.add('row')
+//     for(let i = 1; i < 20; i++) {
+//       const cell = document.createElement('div')
+//       cell.classList.add('cell')
+//       row.appendChild(cell)
+//     }
+//     mainGrid.appendChild(row)
+//   }
+// } else {
+//   console.error("Main grid element not found");
+// }
